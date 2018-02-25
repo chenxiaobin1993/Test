d1ec831cff6e0384f52ac01fc376549d92b354ee
bbchen3 <bbchen3@iflytek.com>
2018-02-25 11:43:54 +0800

    foreach循环的null测试

---
+++ src/main/java/forcirculate/Test.java
@@ -0,0 +1,14 @@
+package forcirculate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+    public static void main(String[] args) {
+        List<Integer> integerList = null;
+        for (Integer integers : integerList) {
+            System.out.println("test");
+        }
+    }
+}
